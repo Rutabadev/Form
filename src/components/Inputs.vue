ee59f020964441428a0f8430b6de0f09a5219862
Rutabadev <etienner37@gmail.com>
2020-01-02 18:49:23 +0100

    setup components and layouts

---
+++ src/components/Inputs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="inputs">
+    <h3>Inputs</h3>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Inputs extends Vue {
+  private msg: string = 'Ok les inputs';
+}
+</script>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="game">
     <h1>Game</h1>
+    <Form></Form>
   </div>
 </template>
+
+<script>
+import Form from '@/components/Form.vue'
+
+export default {
+  name: 'game',
+  components: {
+    Form
+  }
+}
+</script>
