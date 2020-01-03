59f9200888e945ec5a6b85f34108f9cbb58d70ab
Etienne Raby <etienne.raby@fittingbox.com>
2020-01-03 09:45:21 +0100

    linted [ci-skip]

--- tests/e2e/specs/routes.js
+++ tests/e2e/specs/routes.js
@@ -1,21 +1,21 @@
 describe('Check routes', () => {
-    it('Visits the app root url', () => {
-        cy.visit('/')
-        cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
-    })
+  it('Visits the app root url', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+  })
 
-    it('Visits \'game\'', () => {
-        cy.visit('/game')
-        cy.contains('h1', 'Game')
-    })
+  it('Visits \'game\'', () => {
+    cy.visit('/game')
+    cy.contains('h1', 'Game')
+  })
 
-    it('Visits \'highscores\'', () => {
-        cy.visit('/highscores')
-        cy.contains('h1', 'Highscores')
-    })
+  it('Visits \'highscores\'', () => {
+    cy.visit('/highscores')
+    cy.contains('h1', 'Highscores')
+  })
 
-    it('Visits \'about\'', () => {
-        cy.visit('/about')
-        cy.contains('h1', 'This is an about page')
-    })
+  it('Visits \'about\'', () => {
+    cy.visit('/about')
+    cy.contains('h1', 'This is an about page')
+  })
 })
