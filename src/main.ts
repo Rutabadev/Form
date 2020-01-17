91bb27d74027aeb96523445a9e07f89447560fea
etienne.raby <etienne.raby@fittingbox.com>
2020-01-17 11:55:45 +0100

    added firebase config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDcEqFaxz969Ve5inMbfRI7qodcZPD6hVo',
+  authDomain: 'form-79a24.firebaseapp.com',
+  databaseURL: 'https://form-79a24.firebaseio.com',
+  projectId: 'form-79a24',
+  storageBucket: 'form-79a24.appspot.com',
+  messagingSenderId: '201666686554',
+  appId: '1:201666686554:web:2f1a246b66c4e9104ab263',
+  measurementId: 'G-PHD843ZHNF'
+}
+
+firebase.initializeApp(firebaseConfig)
 
 Vue.config.productionTip = false
 
